3ecc1c5303324ea51a2b702f634fb86f86618a07
Anuraag Agrawal <anuraaga@gmail.com>
2022-12-05 12:25:59 +0900

    gctrace=1

--- re2.go
+++ re2.go
@@ -750,7 +750,6 @@ func (re *Regexp) MatchString(s string) bool {
 func (re *Regexp) release() {
 	re.abi.startOperation(0)
 	defer re.abi.endOperation()
-	fmt.Printf("releasing: %p\n", re)
 	release(re)
 }
 
