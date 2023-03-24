95cc3524f76bfff34605e7a40c104ed08b8438a8
Anuraag Agrawal <anuraaga@gmail.com>
2023-03-24 16:36:46 +0900

    Remove outdated comment

--- experimental/experimental.go
+++ experimental/experimental.go
@@ -8,8 +8,6 @@ import (
 // CompileLatin1 is like regexp.Compile but causes the matching to treat
 // the input as arbitrary bytes rather than unicode strings. This is
 // similar behavior to the rsc.io/binaryregexp package.
-//
-// Currently Longest and Copy are not supported with latin1.
 func CompileLatin1(expr string) (*re2.Regexp, error) {
 	return internal.Compile(expr, internal.CompileOptions{Latin1: true})
 }
