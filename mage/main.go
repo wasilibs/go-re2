085bbd6b6f9bf21d3ac3ce8b4cdfc3ce8629f786
Anuraag Agrawal <anuraaga@gmail.com>
2024-05-01 16:00:07 +0900

    git add

---
+++ mage/main.go
@@ -0,0 +1,13 @@
+// Entrypoint to mage for running without needing to install the command.
+// https://magefile.org/zeroinstall/
+package main
+
+import (
+	"os"
+
+	"github.com/magefile/mage/mage"
+)
+
+func main() {
+	os.Exit(mage.Main())
+}
